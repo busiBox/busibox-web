636bf2b6fa0de9e0684c9f53cb7a7e9d232750d5
Gustavo Batistuti <gustavo.batistuti@bandtec.com.br>
2020-07-26 23:06:46 -0300

    Removing temporarily redux dependencies

--- src/store/ducks/login/actions.ts
+++
@@ -1,9 +0,0 @@
-import { action } from "typesafe-actions";
-import { LoginTypes, Login } from "./types";
-
-export const loadRequest = () => action(LoginTypes.LOAD_REQUEST);
-
-export const loadSuccess = (data: Login) =>
-  action(LoginTypes.LOAD_SUCCESS, data);
-
-export const loadFailure = () => action(LoginTypes.LOAD_FAILURE);
